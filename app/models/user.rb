c785e64449c465bf7f2067215963c8e891c4f38a
Thiago Barbosa <thiagobarbosa48@gmail.com>
2021-04-01 11:19:32 -0300

    Models and Controllers Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   mount_uploader :avatar, AvatarUploader
+
+  has_many :posts
+  has_many :likes
+
+  acts_as_followable
+  acts_as_follower
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  resources :likes, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy, :edit, :update]
+  resources :posts, only: [:index, :show, :create, :destroy, :edit, :update]
+  resources :follows, only: [:create, :destroy]
+
+  get '/profile/:id', to: "users#show", as: "user"
+  get '/profile/:id/followings', to: "users#followings", as: "following"
+  get '/profile/:id/followers', to: "users#followers", as: "followers"
+  post '/users/search', to: "users#show", as: :search
+  get '/users/search', to: "users#show"
+
   devise_for :users, :controllers => {registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
